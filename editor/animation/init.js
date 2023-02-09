14f28ad224562996c370373fa26a11d53c98065f
Oleksandr Zozulia <freemanlex@gmail.com>
2023-02-09 23:16:42 +0200

    next-api, uk

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,31 +1,6 @@
-requirejs(['ext_editor_1', 'ext_editor_io', 'jquery_190', 'raphael_210'],
-    function (ext, extIO, $, TableComponent) {
-        var io = new extIO({
-            functions: {
-                js: 'friendlyNumber',
-                python: 'friendly_number'
-            }
-        });
-        io.parseInputArguments = function (checkioInput) {
-            var lang = top.Ext.INTERPRETER_SLUG.split('-')[0];
-            if (lang === 'js') {
-                var args = ext.JSON.stringify(checkioInput);
-                return args.slice(1, args.length - 1);
-            } else {
-                var checkioInputStr = checkioInput[0];
-
-                var pow = String(checkioInputStr).match(/(?:\de\+)(\d+)/);
-                if (pow && pow.length > 1) {
-                    checkioInputStr = "10**" + pow[1];
-                }
-
-
-                for(var kwarg in checkioInput[1]){
-                    checkioInputStr += ', ' + kwarg + '=' + JSON.stringify(checkioInput[1][kwarg]);
-                }
-                return checkioInputStr;
-            }
-        };
+requirejs(['ext_editor_io2', 'jquery_190'],
+    function (extIO, $) {
+        var io = new extIO({});
         io.start();
     }
 );
--- verification/tests.py
+++ verification/tests.py
@@ -8,57 +8,57 @@ Each test is dict with
 """
 
 TESTS = {
-    "1. Base": [
+    "Basics": [
         {
             "input": [102, {}],
-            "answer": '102'
+            "answer": '102',
         },
         {
             "input": [12341234, {'decimals': 1}],
-            "answer": '12.3M'
+            "answer": '12.3M',
         },
         {
             "input": [12000000, {'decimals': 3}],
-            "answer": '12.000M'
+            "answer": '12.000M',
         },
         {
             "input": [102, {'decimals': 2}],
-            "answer": '102.00'
+            "answer": '102.00',
         },
         {
             "input": [10240, {}],
-            "answer": '10k'
+            "answer": '10k',
         },
         {
             "input": [1024000000, {'base': 1024, 'suffix': 'iB'}],
-            "answer": '976MiB'
+            "answer": '976MiB',
         },
         {
             "input": [-150, {'base': 100, "powers": ['', 'd', 'D']}],
-            "answer": '-1d'
+            "answer": '-1d',
         },
         {
             "input": [-155, {'base': 100, "decimals": 1, "powers": ['', 'd', 'D']}],
-            "answer": '-1.6d'
+            "answer": '-1.6d',
         },
         {
             "input": [255000000000, {"powers": ['', 'k', 'M']}],
-            "answer": '255000M'
+            "answer": '255000M',
         },
     ],
-    "2. Edges": [
+    "Edges": [
         {
             "input": [1, {}],
-            "answer": '1'
+            "answer": '1',
         },
         {
             "input": [0, {'decimals': 3, 'suffix': "th"}],
-            "answer": '0.000th'
+            "answer": '0.000th',
         },
         {
             "input": [10 ** 24, {}],
             "answer": '1Y',
-            "explanation": "10**24"
+            "explanation": "10**24",
         },
         {
             "input": [10 ** 32, {}],
@@ -67,33 +67,33 @@ TESTS = {
         },
         {
             "input": [4294967297, {'base': 2, 'powers': ["p" + str(n) for n in range(32)]}],
-            "answer": '2p31'
+            "answer": '2p31',
         },
         {
             "input": [1, {'decimals': 15}],
-            "answer": '1.000000000000000'
+            "answer": '1.000000000000000',
         },
     ],
-    "3. Extra": [
+    "Extra": [
         {
             "input": [42, {'base': 10, 'powers': ["u", "d"]}],
-            "answer": '4d'
+            "answer": '4d',
         },
         {
             "input": [42, {'powers': ["u", "d"], "suffix": "-n"}],
-            "answer": '42u-n'
+            "answer": '42u-n',
         },
         {
             "input": [19821904, {}],
-            "answer": '19M'
+            "answer": '19M',
         },
         {
             "input": [4000000001, {"base": 1024, "decimals": 1}],
-            "answer": '3.7G'
+            "answer": '3.7G',
         },
         {
             "input": [9000, {"suffix": "iB"}],
-            "answer": '9kiB'
+            "answer": '9kiB',
         },
     ]
 }
